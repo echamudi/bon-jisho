05ff792309b2f978b22b3335ff750ccdf16a9bf7
Ezzat Chamudi <chamudi.git@outlook.com>
2020-05-19 08:06:18 +0300

    rename jmdict entry to dict processor

--- ng-src/app/blocks/entry-details/entry-details.component.ts
+++ ng-src/app/blocks/entry-details/entry-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ElectronService } from 'ng-src/app/services/electron.service';
-import { getAllKanjiReadingPairs } from 'lib/jmdict-entry';
+import { getAllKanjiReadingPairs } from 'lib/dict-processor';
 import { JMdict, JapaneseDB } from 'japanese-db';
 import { getJMdictJsonsRows } from 'src/main/db';
 
