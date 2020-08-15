6e6ebc08d3e18c73b686368058adea7f4868e606
Ezzat Chamudi <echamudi@outlook.com>
2020-08-15 22:33:06 +0300

    Rename directories

--- ng-src/app/app.module.ts
+++ ng-src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './views/home/home.component';
-import { SidebarComponent } from './blocks/sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AdvancedSearchComponent } from './views/advanced-search/advanced-search.component';
-import { ToolbarComponent } from './blocks/toolbar/toolbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { BasicSearchComponent } from './views/basic-search/basic-search.component';
-import { EntryDetailsComponent } from './blocks/entry-details/entry-details.component';
+import { EntryDetailsComponent } from './components/entry-details/entry-details.component';
 
 import { ElectronService } from './services/electron.service';
 import { TooltipModule } from 'ng2-tooltip-directive';
