45b4844e9e1706df315a0e91cc17361e491d75bb
Ezzat Chamudi <echamudi@outlook.com>
2020-08-16 02:02:15 +0300

    Fix urls

--- ng-src/app/app-routing.module.ts
+++ ng-src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from 'ng-src/app/views/home/home.component';
-import { AdvancedSearchComponent } from 'ng-src/app/views/advanced-search/advanced-search.component';
-import { BasicSearchComponent } from 'ng-src/app/views/basic-search/basic-search.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdvancedSearchComponent } from './components/advanced-search/advanced-search.component';
+import { BasicSearchComponent } from './components/basic-search/basic-search.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
