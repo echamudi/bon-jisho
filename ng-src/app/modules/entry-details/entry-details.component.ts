1c9ed9c8f9951d118de0064ffb6c1b256e420860
Ezzat Chamudi <echamudi@outlook.com>
2021-07-04 23:11:00 +0700

    Add easter egg comment

--- ng-src/app/modules/entry-details/entry-details.component.ts
+++ ng-src/app/modules/entry-details/entry-details.component.ts
@@ -57,7 +57,7 @@ export class EntryDetailsComponent implements OnInit {
   mode: Mode | null = null;
 
   /**
-   * For easter egg
+   * For easter egg, showing raw json
    */
   exploreClickCount: number = 0;
 
